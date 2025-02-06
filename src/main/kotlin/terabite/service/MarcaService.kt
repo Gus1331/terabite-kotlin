99e78fd1df984bc372823137445845447985feae
Gus1331 <gustavooliver.souza@gmail.com>
2025-02-06 18:18:15 -0300

    marcaservice

---
+++ src/main/kotlin/terabite/service/MarcaService.kt
@@ -0,0 +1,52 @@
+package terabite.service
+
+import org.springframework.http.HttpStatusCode
+import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+import terabite.entity.Marca
+import terabite.repository.MarcaRepository
+
+@Service
+class MarcaService(private val marcaRepository: MarcaRepository) {
+    fun listarMarca(): List<Marca>{
+        var marcas: List<Marca> = marcaRepository.findAll()
+
+        if(marcas.isEmpty()){
+            throw ResponseStatusException(HttpStatusCode.valueOf(204))
+        }
+
+        return marcas
+    }
+
+    fun buscarPorId(id:Int): Marca{
+        return marcaRepository.findById(id).orElseThrow{ResponseStatusException(HttpStatusCode.valueOf(404))}
+    }
+
+    fun buscarPorNomeMarca(nomeMarca: String): Marca{
+        var marca = marcaRepository.findByNomeIgnoreCase(nomeMarca)
+
+        if(marca != null){
+            return  marca
+        }
+
+        return criarMarca(Marca(null,  nomeMarca))
+    }
+
+    fun criarMarca(novaMarca: Marca): Marca{
+        novaMarca.id = null
+        return marcaRepository.save(novaMarca)
+    }
+
+    fun atualizarMarca(id: Int, marca: Marca): Marca{
+        if(!marcaRepository.existsById(id)){ throw ResponseStatusException(HttpStatusCode.valueOf(404))}
+
+        marca.id = id
+        return marcaRepository.save(marca);
+    }
+
+    fun deletarMarca(id: Int){
+        if(!marcaRepository.existsById(id)){ throw ResponseStatusException(HttpStatusCode.valueOf(404))}
+
+        marcaRepository.deleteById(id)
+    }
+}
