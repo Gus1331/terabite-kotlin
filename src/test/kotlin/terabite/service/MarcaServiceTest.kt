7badcc899c11c35b7ed15104e96feca54a752232
Gus1331 <gustavooliver.souza@gmail.com>
2025-02-12 21:41:24 -0300

    ADD MarcaServiceTest testes de validação de nome de marca

--- src/test/kotlin/terabite/service/MarcaServiceTest.kt
+++ src/test/kotlin/terabite/service/MarcaServiceTest.kt
@@ -7,13 +7,17 @@ import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.whenever
 import org.springframework.http.HttpStatus
 import org.springframework.web.server.ResponseStatusException
 import terabite.entity.Marca
 import terabite.repository.MarcaRepository
+import java.util.*
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.fail
 
 @ExtendWith(MockitoExtension::class)
 @DisplayName("Marca")
@@ -45,5 +49,85 @@ class MarcaServiceTest{
         val exception: ResponseStatusException = assertThrows<ResponseStatusException> { methodCall() }
 
         assertEquals(HttpStatus.NO_CONTENT, exception.statusCode, "Status HTTP esperado é 204 (NO_CONTENT)")
+    }
+
+    @Test
+    @DisplayName("Quando o banco de dados possui marcas, o serviço deve retornar a lista correta")
+    fun deveRetornarListaDeMarcasQuandoExistirem(){
+        whenever(marcaRepository.findAll()).thenReturn(marcasEsperadas)
+
+        val marcasRetornadas: MutableList<Marca> = mutableListOf<Marca>()
+
+        try {
+            marcasRetornadas.addAll(marcaService.listarMarca())
+        } catch (e: Exception){
+            fail("Erro ao buscar marca: " + e.message)
+        }
+
+        assertEquals(marcasEsperadas, marcasRetornadas, "As listas retornadas não são iguais às esperadas")
+    }
+
+    @Test
+    @DisplayName("Quando buscar por ID inexistente, deve lançar ResponseStatusException com status 404 (NOT_FOUND)")
+    fun deveLancarExcecaoQuandoNaoEncontrarMarcaPorID(){
+        val methodCall: () -> Marca = {marcaService.buscarPorId(2)}
+        val exception: ResponseStatusException = assertThrows<ResponseStatusException> { methodCall() }
+
+        assertEquals(HttpStatus.NOT_FOUND, exception.statusCode, "O status HTTP esperado é 404 (NOT_FOUND)")
+    }
+
+    @Test
+    @DisplayName("Quando buscar por Id existente, deve retornar a marca correspondente")
+    fun deveRetornarMarcaQuandoPassadoSeuId(){
+        val marcaEsperada: Marca = marcasEsperadas[0]
+
+        whenever(marcaRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(marcaEsperada))
+
+        val resultado: Marca = marcaService.buscarPorId(1)
+
+        assertNotNull(resultado, "A marca retornada não deveria ser nula")
+        assertEquals(marcaEsperada.id, resultado.id, "O ID da marca retornada não está correto")
+        assertEquals(marcaEsperada.nome, resultado.nome, "O nome da marca retornada não está correto")
+    }
+
+    @Test
+    @DisplayName("Quando buscar por marca isBlanck, deve lançar exceção 400 (BAD_REQUEST)")
+    fun deveLancarExcecaoQuandoPassarNomeVazio(){
+        val methodCall: () -> Marca = {marcaService.buscarPorNomeMarca(" ")}
+        val exception: ResponseStatusException = assertThrows<ResponseStatusException> { methodCall() }
+
+        assertEquals(HttpStatus.BAD_REQUEST, exception.statusCode, "O status HTTP esperado é 400 (BAD_REQUEST)")
+    }
+
+    @Test
+    @DisplayName("Quando passar uma marca que não existe no banco de dados, deve cadastrar uma nova marca")
+    fun deveCriarNovaMarcaQuandoBuscarPorNomeNaoExistente(){
+        val novaMarca:String = "Nova Marca"
+
+        whenever(marcaRepository.findByNomeIgnoreCase(Mockito.anyString())).thenReturn(null)
+
+        val resposta: Marca = marcaService.buscarPorNomeMarca(novaMarca)
+
+        assertNotNull(resposta)
+        assertEquals(resposta.nome, novaMarca, "O nome da marca não foi criado corretamente")
+    }
+
+    @Test
+    @DisplayName("Quando buscar por nome existente em diferentes cases, deve retornar a marca correspondente")
+    fun deveRetornarMarcaQuandoPassadoNomeComCasesDiferentes(){
+        val marcaEsperada: Marca = Marca(1, "Señor Sorvete")
+
+        whenever(marcaRepository.findByNomeIgnoreCase(Mockito.anyString())).thenReturn(marcaEsperada)
+        val resultadoUpperCase: Marca = marcaService.buscarPorNomeMarca("SEÑOR SORVETE")
+        val resultadoLowerCase: Marca = marcaService.buscarPorNomeMarca("señor sorvete")
+        val resultadoMixedCase: Marca = marcaService.buscarPorNomeMarca("SeÑoR sORVeTE")
+
+        // val validarId: (id1: Int, id2: Int) -> Unit = { assertEquals(id1, id2) }
+
+        assertNotNull(resultadoUpperCase, "A marca retornada com nome em UPPERCASE não deveria ser nula")
+        assertNotNull(resultadoLowerCase, "A marca retornada com nome em lowercase não deveria ser nula")
+        assertNotNull(resultadoMixedCase, "A marca retornada com nome em MixedCase não deveria ser nula")
+
+        assertEquals(marcaEsperada.id, resultadoUpperCase.id, "O ID da marca retornada (UPPERCASE) não está correto\"")
     }
 }
