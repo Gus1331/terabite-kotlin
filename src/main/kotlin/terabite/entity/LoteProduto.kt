9c1d42193210b03a4c7bf7a99f2bc2664c3e6841
Gus1331 <gustavooliver.souza@gmail.com>
2025-03-27 23:23:18 -0300

    feat: loteProduto entity

---
+++ src/main/kotlin/terabite/entity/LoteProduto.kt
@@ -0,0 +1,21 @@
+package terabite.entity
+
+import jakarta.persistence.*
+
+data class LoteProduto(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_LP")
+    private var id: Int? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "FK_ID_LOTE_LP", referencedColumnName = "ID_LOTE")
+    private val lote: Lote? = null,
+
+    @ManyToOne
+    @JoinColumn(name = "FK_ID_PROD_LP", referencedColumnName = "ID_PROD")
+    private val produto: Produto? = null,
+
+    @Column(name = "QTD_CAIXAS_COMPRADAS_LP")
+    private var qtdCaixasCompradas: Int? = null
+)
